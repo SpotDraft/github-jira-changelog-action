0e2d8f83da80edacf810e71961390371e550c83b
dheerajmav <dheeraj.mavilla@spotdraft.com>
2023-12-21 15:57:18 +0530

    update

--- index.js
+++ index.js
@@ -145,7 +145,7 @@ async function main() {
     const changelog = await jira.generate(commitLogs, release);
     if (changelog.errors) {
       const jiraError = changelog.errors[0];
-      if (jiraError.statusCode === 404) {
+      if (jiraError.statusCode == 404) {
         console.error(`Jira Ticket Not Found: ${jiraError.message}`);
       } else {
         throw new Error(`Jira API Error: ${JSON.stringify(changelog.errors)}`);
